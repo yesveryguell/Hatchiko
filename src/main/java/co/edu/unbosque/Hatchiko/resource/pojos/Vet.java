9f125cfe881935f5302d2745bbf8e1fb70cae44c
Omar Felipe Ladino Guezguan <oladinog@unbosque.edu.co>
2021-06-05 19:05:24 -0500

    Adding pojos UserApp and Vet

--- src/main/java/co/edu/unbosque/Hatchiko/resource/pojos/Vet.java
+++ src/main/java/co/edu/unbosque/Hatchiko/resource/pojos/Vet.java
@@ -1,4 +1,51 @@
 package co.edu.unbosque.Hatchiko.resource.pojos;
 
 public class Vet {
+
+    private String username;
+    private String name;
+    private String adress;
+    private String neighborhood;
+
+    public Vet() {
+    }
+
+    public Vet(String username, String name, String adress, String neighborhood) {
+        this.username = username;
+        this.name = name;
+        this.adress = adress;
+        this.neighborhood = neighborhood;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
 }
