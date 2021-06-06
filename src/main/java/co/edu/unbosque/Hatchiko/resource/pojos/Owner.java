d1f0c9d02edaa7a885f006025ff202a0a46d1699
Omar Felipe Ladino Guezguan <oladinog@unbosque.edu.co>
2021-06-05 19:00:53 -0500

    Adding pojos owner and pet

--- src/main/java/co/edu/unbosque/Hatchiko/resource/pojos/Owner.java
+++ src/main/java/co/edu/unbosque/Hatchiko/resource/pojos/Owner.java
@@ -1,4 +1,50 @@
 package co.edu.unbosque.Hatchiko.resource.pojos;
 
 public class Owner {
+    private String username;
+    private String name;
+    private String adress;
+    private String neighborhood;
+
+    public Owner() {
+    }
+
+    public Owner(String username, String name, String adress, String neighborhood) {
+        this.username = username;
+        this.name = name;
+        this.adress = adress;
+        this.neighborhood = neighborhood;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
 }
--- src/main/java/co/edu/unbosque/Hatchiko/resource/pojos/Pet.java
+++ src/main/java/co/edu/unbosque/Hatchiko/resource/pojos/Pet.java
@@ -1,4 +1,91 @@
 package co.edu.unbosque.Hatchiko.resource.pojos;
 
 public class Pet {
+
+    private int pet_id;
+    private String microchip;
+    private String name;
+    private String species;
+    private String race;
+    private String size;
+    private String sex;
+    private String picture;
+
+    public Pet() {
+    }
+
+    public Pet(int pet_id, String microchip, String name, String species, String race, String size, String sex, String picture) {
+        this.pet_id = pet_id;
+        this.microchip = microchip;
+        this.name = name;
+        this.species = species;
+        this.race = race;
+        this.size = size;
+        this.sex = sex;
+        this.picture = picture;
+    }
+
+    public int getPet_id() {
+        return pet_id;
+    }
+
+    public void setPet_id(int pet_id) {
+        this.pet_id = pet_id;
+    }
+
+    public String getMicrochip() {
+        return microchip;
+    }
+
+    public void setMicrochip(String microchip) {
+        this.microchip = microchip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
 }
