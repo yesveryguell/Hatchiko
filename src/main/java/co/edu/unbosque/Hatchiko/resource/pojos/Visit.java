b9ec3120168efdc92a8ca1179ed8a0f6dea8df15
Omar Felipe Ladino Guezguan <oladinog@unbosque.edu.co>
2021-06-05 19:08:12 -0500

    Adding pojo Visit

--- src/main/java/co/edu/unbosque/Hatchiko/resource/pojos/Visit.java
+++ src/main/java/co/edu/unbosque/Hatchiko/resource/pojos/Visit.java
@@ -1,4 +1,51 @@
 package co.edu.unbosque.Hatchiko.resource.pojos;
 
 public class Visit {
+
+    private int visit_id;
+    private String created_at;
+    private String type;
+    private String description;
+
+    public Visit() {
+    }
+
+    public Visit(int visit_id, String created_at, String type, String description) {
+        this.visit_id = visit_id;
+        this.created_at = created_at;
+        this.type = type;
+        this.description = description;
+    }
+
+    public int getVisit_id() {
+        return visit_id;
+    }
+
+    public void setVisit_id(int visit_id) {
+        this.visit_id = visit_id;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
